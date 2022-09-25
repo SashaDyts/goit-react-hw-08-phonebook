ddc3b302912ea9456454d42bad99a48a223380c5
SashaDyts <lentrofan@gmail.com>
2022-09-25 18:33:29 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,12 +2,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getUserEmail } from 'redux/auth/auth-selectors';
 import { Container, Button } from './UserMenu.styled';
 import { logout } from 'redux/auth/auth-operations';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 export default function UserMenu() {
   const userEmail = useSelector(getUserEmail);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
+  //   const navigate = useNavigate();
 
   return (
     <Container>
